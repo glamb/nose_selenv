94f95d5ec2f9b025a12900cc0de5c6b89b2e8e8c
Gregg Lamb <greggmlamb@gmail.com>
2014-06-09 22:37:29 -0700

    cleanig up

--- nose_selenv.py
+++ nose_selenv.py
@@ -1,56 +1,65 @@
 import os
 from nose.plugins import Plugin
-import ConfigParser
+from ConfigParser import ConfigParser
 
-def parse_config_file(config):
-	if config.has_option('SELENIUM', 'BROWSER'):
-		os.environ['SELENV_BROWSER'] = config.get('SELENIUM','BROWSER')
-	else:
-		os.environ['SELENV_BROWSER'] = 'PHANTOMJS'
-	if config.has_option('SELENIUM', 'ENVIRONMENT'):
-		os.environ['SELENV_ENVIRONMENT'] = config.get('SELENIUM', 'ENVIRONMENT')
-	else:
-		os.environ['SELENV_ENVIRONMENT'] = 'develop'
+def set_options_from_config(config):
+    if config.has_option('SELENIUM', 'ENVIRONMENT'):
+	    os.environ['SELENV_ENVIRON'] = config.get('SELENIUM','ENVIRONMENT')
+    else:
+	    os.environ['SELENV_ENVIRON'] = 'develop'
+
+    if config.has_option('SELENIUM', 'BROWSER'):
+	    os.environ['SELENV_BROWSER'] = config.get('SELENIUM', 'BROWSER')
+    else:
+	    os.environ['SELENV_BROWSER'] = 'PHANTOMJS'
 
 class SelEnv(Plugin):
-	name = 'selenv'
-	enabled = True
-	def option(self, parser, env=os.environ):
-		Plugin.options(self, parser, env)
 
-		parser.add_option('--browser',
-				action='store',
-				default=env.get('SELENV_BROWSER', 'PHANTOMJS'),
-				dest='browser',
-				help='The base url for the application in test.(default: %default)'
-				)
-		valid_env_options = ['develop', 'staging', 'dogfood', 'production']
-		parse.add_option('--env',
-				action='store',
-				choices=valid_env_options,
-				default=env.get('SELENV_ENVIRONMENT', 'develop'),
-				dest='environment',
-				help='Environment to run the test against. (default options: )' 
-				)
-		parse.add_option('--config-file',
-				action='store',
-				dest='config_file',
-				help='Load a config file instead of using nose params.'
-				)
-	
-	def get_config_file(self, config_file):
-		config = ConfigParser()
-		config.read(config_file)
-		parse_config_file(config)
+    name = 'selenv'
+    enabled = True
 
-	def set_options(self, options):
-		os.environ['SELENV_BROWSER'] = option.browser
-		os.environ['SELENV_ENVIRONMENT'] = option.environment
+    def _stringify_options(self, list):
+        string = ", ".join(list)
+        return "[" + string + "]"
 
-	def configure(self, options, conf):
-		Plugin.configure(self, options, conf)
-		if self.enabled:
-			if options.config_file:
-				self.get_config_file(options.config_file)
-			else:
-				self.set_options(options)
+    def options(self, parser, env=os.environ):
+
+        Plugin.options(self, parser, env)
+        parser.add_option('--config-file',
+                          action='store',
+                          dest='config_file',
+                          help='Load options from a config file instead of enter via the command line.'
+        )
+        valid_location_options = ['develop', 'staging', 'dogfood', 'production']
+        parser.add_option('--env',
+                          action='store',
+                          choices=valid_location_options,
+                          default=env.get('SELENV_ENVIRO', 'develop'),
+                          dest='environment',
+                          help='Run the browser in this location (default %default, options ' +
+                               self._stringify_options(valid_location_options) +
+                               ').' 
+        )
+        parser.add_option('--browser',
+                          action='store',
+                          default=env.get('SELENV_BROWSER', 'PHANTOMJS'),
+                          dest='browser',
+                          help="Select the type of browser you want Selenium to use. (default %default). "
+        )
+
+    def read_config_file(self, config_file):
+        CONFIG = ConfigParser()
+        CONFIG.read(config_file)
+        set_options_from_config(CONFIG)
+
+    def set_options(self, options):
+	os.environ['SELENV_ENVIRON'] = options.environment
+	os.environ['SELENV_BROWSER'] = options.browser
+
+    def configure(self, options, conf):
+        Plugin.configure(self, options, conf)
+        if self.enabled:
+            if options.config_file:
+                self.read_config_file(options.config_file)
+            else:
+                self.set_options(options)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 		name='nose_selenv',
-		version='0.1',
+		version='0.1.0',
 		author='Gregg Lamb',
 		author_email='glamb@wetpaint-inc.com',
 		description='Nose plugin for wetpaint web qa',
